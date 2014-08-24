5bdc84a6aa161d27af1db40eaa73ca359a8e17a6
Mladen <mlgrozev@gmail.com>
2014-08-24 19:15:43 +0300

    basic home page

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcome to larabook!</h1>
+            <p>Welcome to laravel Larabook!</p>
+            <p>To see the difference between static and fixed top navbars, just scroll.</p>
+            <p>
+              <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign Up!</a>
+            </p>
+          </div>
+@stop
